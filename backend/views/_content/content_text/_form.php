0208627eb7e70e22f68e3ba41e1b72daccd72fec
Saiat Kalbiev <kalbievich11@gmail.com>
2018-09-14 14:09:49 +0400

    Implement apollo11/lobi-cms-core/FileInput instead of kartik/FileInput to content_text view page

--- backend/views/_content/content_text/_form.php
+++ backend/views/_content/content_text/_form.php
@@ -1,12 +1,13 @@
 <?php
 
-use kartik\file\FileInput;
+use apollo11\lobicms\widgets\FileInput;
 use backend\widgets\LanguageSelector;
 use yii\helpers\Html;
 
 /**
  * @var $this  yii\web\View
  */
+
 ?>
 
 <?php echo $form->field($model, 'language')->widget(LanguageSelector::class, []) ?>
@@ -19,22 +20,6 @@ use yii\helpers\Html;
     'preset' => 'full'
 ]) ?>
 
-<?php
-$pluginOptions = [
-    'showUpload' => false
-];
-if($model->fileManagerImage && $model->fileManagerImage->name) {
-    $pluginOptions['initialPreview'] = [Html::img($model->fileManagerImage->getUrl(), ['style' => 'height:160px;'])];
-    $pluginOptions['initialCaption'] = [$model->fileManagerImage->name];
-    $pluginOptions['initialPreviewConfig'] = [
-        [
-            'caption' => $model->fileManagerImage ? $model->fileManagerImage->name : '',
-            'size' => $model->fileManagerImage ? $model->fileManagerImage->size : '',
-            'height' => "120px"
-        ]
-    ];
-}
-echo $form->field($model, 'image')->widget(FileInput::class, [
+<?php echo $form->field($model, 'image')->widget(FileInput::class, [
     'options' => ['accept' => 'image/*', 'multiple' => false],
-    'pluginOptions' => $pluginOptions,
-]); ?>
+]);
