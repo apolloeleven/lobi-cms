0294b107b73f673fb8a799b1119b6c730c0cd7ec
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2018-09-14 12:30:41 +0400

    Delete renderIndicators method to render custom styling indicators

--- common/widgets/Carousel.php
+++ common/widgets/Carousel.php
@@ -16,20 +16,4 @@ class Carousel extends \yii\bootstrap\Carousel
 {
     public $controls = ['<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>', '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'];
 
-    public function renderIndicators()
-    {
-        if ($this->showIndicators === false) {
-            return '';
-        }
-        $indicators = [];
-        foreach ($this->items as $i => $item) {
-            $options = ['data-target' => '#' . $this->options['id'], 'data-slide-to' => $i];
-            if ($i === 0) {
-                Html::addCssClass($options, 'active');
-            }
-            $indicators[] = Html::tag('li', $item['content'], $options);
-        }
-
-        return Html::tag('ol', implode("\n", $indicators), ['class' => 'carousel-indicators']);
-    }
 }
