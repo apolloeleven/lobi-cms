4a1cd05ea14c0dbaae94362b4c1101030647f9de
levani <levani19972@gmail.com>
2018-10-23 18:48:53 +0400

    Fix image deletion bug, Imlement new form for image deletion

--- backend/views/_content/website/_form.php
+++ backend/views/_content/website/_form.php
@@ -45,6 +45,25 @@ use yii\helpers\Url;
     'options' => ['rows' => 4],
     'preset' => 'full'
 ]) ?><?php echo $form->field($model, 'footer_copyright')->textInput(['maxlength' => true]) ?>
-<?php echo $form->field($model, 'footer_logo')->widget(FileInput::class, [
-    'options' => ['accept' => 'image/*'],
+<?php
+$pluginOptions = [
+    'showUpload' => false
+];
+if ($model->image && $model->image->name) {
+    echo $form->field($model, 'deletedImages')->hiddenInput();
+    $pluginOptions['initialPreview'] = [Html::img($model->image->getUrl(), ['style' => 'height:160px;'])];
+    $pluginOptions['initialCaption'] = [$model->image->name];
+    $pluginOptions['initialPreviewConfig'] = [
+        [
+            'caption' => $model->image ? $model->image->name : '',
+            'size' => $model->image ? $model->image->size : '',
+            'height' => "120px",
+            'key' => $model->image->id,
+            'url' => Url::to(['base/delete-file-item'])
+        ]
+    ];
+}
+echo $form->field($model, 'image')->widget(FileInput::class, [
+    'options' => ['accept' => 'image/*', 'multiple' => false],
+    'pluginOptions' => $pluginOptions,
 ]); ?>
