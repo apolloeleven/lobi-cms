b1c038f5677e64b55ce8e90395763f5e4425fb75
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2018-11-02 19:58:53 +0400

    Set config to use BackendView

--- backend/config/base.php
+++ backend/config/base.php
@@ -4,6 +4,9 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'urlManager' => require __DIR__ . '/_urlManager.php',
-        'frontendCache' => require Yii::getAlias('@frontend/config/_cache.php')
+        'frontendCache' => require Yii::getAlias('@frontend/config/_cache.php'),
+        'view' => [
+            'class' => \apollo11\lobicms\web\BackendView::class,
+        ],
     ],
 ];
