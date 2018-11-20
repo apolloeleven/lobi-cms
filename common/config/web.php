e63b5fc22484b919c62c2b81b11b539b32e7bb0f
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2018-11-20 16:28:44 +0400

    Remove project specific customStyles

--- common/config/web.php
+++ common/config/web.php
@@ -9,66 +9,6 @@ $config = [
         'ckEditorStyles' => [
             'class' => \common\components\CKEditorComponent::class,
             'customStyles' => [
-//                [
-//                    "name" => 'My UL class',
-//                    "element" => 'ul',
-//                    "attributes" => ['class' => 'some-cool-class']
-//                ],
-//                [
-//                    "name" => 'Another UL class',
-//                    "element" => 'ul',
-//                    "attributes" => ['class' => 'another-cool-class']
-//                ],
-                [
-                    "name" => 'List icon hand',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-hand']
-                ],
-                [
-                    "name" => 'List icon Magnifier',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-magnifier']
-                ],
-                [
-                    "name" => 'List icon Thunderstorm',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-thunderstorm']
-                ],
-                [
-                    "name" => 'List icon Three waves',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-three-waves']
-                ],
-                [
-                    "name" => 'List icon Skull',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-skull']
-                ],
-                [
-                    "name" => 'List icon Medicine',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-medicine']
-                ],
-                [
-                    "name" => 'List icon Bullet',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-bullet']
-                ],
-                [
-                    "name" => 'List icon Baby Crying',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-baby-crying']
-                ],
-                [
-                    "name" => 'List icon Baby Smiles',
-                    "element" => 'ul',
-                    "attributes" => ['class' => 'list-icon-baby-smiles']
-                ],
-                [
-                    "name" => 'Header video bubble',
-                    "element" => 'p',
-                    "attributes" => ['class' => 'bubble']
-                ],
             ]
         ]
     ],
