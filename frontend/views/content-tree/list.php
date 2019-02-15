d62aaa215c6db962b6b32d53311e461f13d4583f
levani <levani19972@gmail.com>
2019-02-15 12:11:38 +0400

    Update list rendering logic

--- frontend/views/content-tree/list.php
+++ frontend/views/content-tree/list.php
@@ -5,12 +5,25 @@
  * Time: 7:01 PM
  */
 
+use frontend\models\ContentTree;
+
 /** @var $this \yii\web\View */
-/** @var $contentTreeItem  \frontend\models\ContentTree */
+/** @var $contentTreeItem  \common\models\ContentTree */
 /** @var $model  \common\models\BaseModel */
 /** @var $viewFile string */
 /** @var $withHidden string */
-$itemsQuery = $contentTreeItem->getItemsQuery()->notDeleted();
+
+if (!isset($tableNames)) {
+    $tableNames = [
+        ContentTree::TABLE_NAME_VIDEO_SECTION,
+        ContentTree::TABLE_NAME_SECTION,
+        ContentTree::TABLE_NAME_CONTENT_TEXT,
+        ContentTree::TABLE_NAME_CAROUSEL,
+        ContentTree::TABLE_NAME_CAROUSEL_ITEM,
+    ]
+}
+
+$itemsQuery = $contentTreeItem->getItemsQuery($tableNames);
 
 (Yii::$app->user->canEditContent() && Yii::$app->request->get('hidden')) ?: $itemsQuery->notHidden();
 
@@ -23,6 +36,7 @@ echo \yii\widgets\ListView::widget([
     'options' => [
         'tag' => false
     ],
+    'emptyText' => false,
     'itemOptions' => [
         'tag' => false,
     ],
@@ -30,7 +44,7 @@ echo \yii\widgets\ListView::widget([
     'itemView' => function ($item, $key, $index, $widget) use ($viewFile, $allItems) {
         /** @var \frontend\models\ContentTree $item */
 
-        return $this->render('item_view',[
+        return $this->render('item_view', [
             'index' => $index,
             'item' => $item,
             'viewFile' => $viewFile,
